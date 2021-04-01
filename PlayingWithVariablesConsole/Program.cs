cbb75b3c53171c929b9eadf5145a70c7dc87b521
Nicholas Wallace <thewallacegames@gmail.com>
2021-04-01 14:39:47 -0500

    PWVCreduex

---
+++ PlayingWithVariablesConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PlayingWithVariablesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// Variable Decleration
+            int age;
+            string name;
+            bool isAlive;
+
+            //Variable Initialization
+            age = 28;
+            name = "Wallace";
+            isAlive = true;
+
+
+            Console.WriteLine($"{name} {age} is he alive: {isAlive}");
+        }
+    }
+}
